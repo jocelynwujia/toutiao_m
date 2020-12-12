be2856b536e8e6f2c7bffd2ca005a96a46fbe7cb
jocelyn <499264478@qq.com>
2020-12-12 22:33:18 +0800

    2.0宫格导航

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -43,6 +43,25 @@
         </div>
       </div>
     </div>
+
+    <!-- 宫格导航 -->
+    <van-grid :column-num="2" class="grid-nav" clickable />
+    <van-grid :column-num="2" class="grid-nav mb-9" clickable>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="iconfont iconshoucang"></i>
+        <span slot="text" class="text">收藏</span>
+      </van-grid-item>
+      <van-grid-item class="grid-item">
+        <i slot="icon" class="iconfont iconlishi"></i>
+        <span slot="text" class="text">历史</span>
+      </van-grid-item>
+    </van-grid>
+    <!-- Cell 单元格 -->
+    <van-cell title="消息通知" is-link />
+    <van-cell class="mb-9" title="小智同学" is-link />
+    <van-cell class="logout-cell" clickable title="退出登录" />
+  </div>
+</template>
   </div>
 
 </template>
@@ -131,6 +150,30 @@ export default {
         }
       }
     }
+  }
+  .grid-nav {
+    .grid-item {
+      height: 141px;
+      i.iconfont {
+        font-size: 45px;
+      }
+      .iconshoucang {
+        color: #eb5253;
+      }
+      .iconlishi {
+        color: #ff9d1d;
+      }
+      span.text {
+        font-size: 28px;
+      }
+    }
+  }
+  .logout-cell {
+    text-align: center;
+    color: #d86262;
+  }
+  .mb-9 {
+    margin-bottom: 9px;
   }
 }
 </style>
