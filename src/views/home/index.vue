8dbd6b051f520252f99b489883ceb7443703a850
jocelyn <499264478@qq.com>
2020-12-13 17:06:47 +0800

    07.首页-头部导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home-container">
-    首页
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button slot="title" size="small" type="info" round icon="search" class="search-btn">搜索</van-button>
+    </van-nav-bar>      
+
+    <!-- 频道列表    -->
+    <!-- v-model 绑定当前激活标签对应的索引号 -->
+    <!-- 通过 animated 属性可以开启切换标签内容时的转场动画 -->
+    <!-- 通过 swipeable 属性可以开启滑动切换标签页。 -->
+    <van-tabs v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -10,7 +24,9 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      active:0
+    }
   },
   computed: {},
   watch: {},
@@ -20,4 +36,18 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+  .home-container{
+    .search-btn {
+      width: 555px;
+      height: 64px;
+      background-color: #5babfb;
+      border: none;
+      font-size: 28px;
+      .vant-icon{
+        font-size: 32px;
+      }
+    }
+    
+  }
+</style>
