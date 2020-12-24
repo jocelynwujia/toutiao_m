c06b085db686da51cb5ee6c0f31052261c7bb5c0
jocelyn <499264478@qq.com>
2020-12-24 09:30:21 +0800

    36.编辑用户资料【修改头像】

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -8,9 +8,18 @@
       @click-left="onClickLeft = $router.back()"
     />
      <!-- /导航栏 -->
-
+     <input 
+     type="file" 
+     hidden 
+     ref="file"
+     @change="onFileChange"
+     > 
      <!-- 个人信息 -->
-     <van-cell title="头像">
+     <van-cell 
+     title="头像"
+     is-link
+     @click="$refs.file.click()"
+     >
        <!-- 头像 -->
        <van-image
         class="avatar"
@@ -77,6 +86,22 @@
       />
      </van-popup>
      <!-- /辑生日弹出层 -->
+
+      <!-- 编辑头像弹出层 -->
+     <van-popup 
+      v-model="isUpdatePhotoShow"
+      position="bottom" 
+      style="height:100%"
+      >
+      <update-photo
+        v-if="isUpdatePhotoShow"
+        :img="img"
+        @close="isUpdatePhotoShow = false"
+        @update-photo="user.photo = $event"
+      />
+      
+     </van-popup>
+     <!-- /编辑头像弹出层 -->
   </div>
 </template>
 
@@ -85,28 +110,33 @@ import {getUserProfile} from '@/api/user'
 import updateName from './components/updateName'
 import UpdateGender from './components/updateGender'
 import UpdateBirthday from './components/updateBirthday'
+import updatePhoto from './components/updatePhoto'
 export default {
   name:'UserProfile',
   components:{
     updateName,
     UpdateGender,
-    UpdateBirthday
+    UpdateBirthday,
+    updatePhoto
   },
   props:{
-    
+
   },
   data(){
     return{
       user:{}, //用户基本信息
       isUpdateNameshow:false,
       isUpdateGenderShow:false,
-      isUpdateBirthdayShow:false
+      isUpdateBirthdayShow:false,
+      isUpdatePhotoShow:false,
+      img:null //预览的图片
     }
   },
   created(){
     this.loadUserProfile()
   },
   methods:{
+    // 获取用户个人信息
     async loadUserProfile(){
       try{
         const {data} = await getUserProfile()
@@ -116,6 +146,21 @@ export default {
         this.$toast.fail('获取失败')
       }
       
+    },
+    onFileChange(){
+      // 获取文件对象
+      const file = this.$refs.file.files[0]
+      // 基于文件对象获取blob数据
+      // const data = window.URL.createObjectURL(file)
+      // console.log(data)
+      // this.img = data
+        this.img=window.URL.createObjectURL(file)
+      // console.log(this.img)
+      // 展示预览图片弹出层
+      this.isUpdatePhotoShow = true
+      // file-input如果选了同一个文件不会触发changge
+      // 解决方案就是每次使用完毕，把它的value清空
+      this.$refs.file.value=''
     }
   }
 }
